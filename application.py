b85005695f55ba710ec23923d411f42985c72e22
Evan Pavone <evanrpavone@gmail.com>
2021-06-07 12:13:03 -0400

    Basics with Variables

--- application.py
+++ application.py
@@ -1,3 +1,27 @@
-msg = "Hello World"
-print(msg)
+# 1. MY first program
 
+print("Hello World")
+
+# 2. Basics of Variables
+
+# Method (print) - prints the msg variable value
+print('This is the next statement')
+# Runs Line by line
+# Variable is a container
+number = 1997
+# = is for assignment
+print(number)
+weight = 120
+
+answer = number + weight
+print(answer)
+
+first_name = "Evan "
+last_name = "Pavone"
+
+sentence = first_name + last_name + " was born in " + str(number) + " and weighs " + str(weight) + " lbs"
+name = first_name + last_name
+# concatenation 
+print(name)
+print(sentence)
+# str(number) converts the integer into a string
