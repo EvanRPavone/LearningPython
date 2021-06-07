5186cfde2847e2dd9f16b68fff8e61532a075780
Evan Pavone <evanrpavone@gmail.com>
2021-06-07 12:38:38 -0400

    Basic Datatypes in Python

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ print("Hello World")
 
 # 2. Basics of Variables
 
+# A Variable is a pointer to a given value
+# A reserved memory location that stores a value
 # Method (print) - prints the msg variable value
 print('This is the next statement')
 # Runs Line by line
@@ -25,3 +27,23 @@ name = first_name + last_name
 print(name)
 print(sentence)
 # str(number) converts the integer into a string
+
+# 3. Basic Datatypes in Python
+
+# Integer(number), String(Sentence), Boolean(True/False), Float(Decimal)
+adult = True # boolean
+data_type_int = type(12)
+data_type_float = type(12.9)
+data_type_str = type("Hello World")
+data_type_boolean = type(adult)
+data_type_from_variable = type(number)
+print(data_type_int)
+# -> <class 'int'> -> Saying that 12 is an integer
+print(data_type_float)
+# -> <class 'float'>
+print(data_type_str)
+# -> <class 'str'>
+print(data_type_boolean)
+# -> <class 'bool'>
+print(data_type_from_variable)
+# -> <class 'int'>
