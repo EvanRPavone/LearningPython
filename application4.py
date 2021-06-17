2ba6e67645ea380fbdaa6948a0c1c8cf97d756bd
Evan Pavone <evanrpavone@gmail.com>
2021-06-17 11:29:48 -0400

    For Loops

--- application4.py
+++ application4.py
@@ -50,3 +50,61 @@ elif animal == "monkey" or animal == "ape":
 else:
     print("We don't know what the animal eats")
 
+# 3. For Loops
+
+# Running a code over, over, and over again
+
+farm_animals = ["goat", "horse", "chicken", "cow", "dog"]
+
+for animal in farm_animals:
+    print(animal)
+    """
+    This will print:
+    goat
+    horse
+    chicken
+    cow
+    dog
+    """
+    sentence = animal + " is safe in our farm"
+    print(sentence)
+    """
+    goat is safe in our farm
+    horse is safe in our farm
+    chicken is safe in our farm
+    cow is safe in our farm
+    dog is safe in our farm
+    
+    NOTE: Will not actually print like this... it will print goat then goat is safe in our farm, horse then horse is safe in our farm etc...
+    """
+    
+touple_of_names = ('Evan', 'Rob', 'Jerry', 'Kevin', 'Griffin', 'Bryce')
+counter = 0
+
+for name in touple_of_names:
+    if "Bryce" in name:
+        print("Bryce is not allowed inside")
+        continue
+    counter = counter + 1
+    sentence = str(counter) + ". " + name + " is allowed inside."
+    print(sentence)
+    """
+    Evan is allowed inside
+    Rob is allowed inside
+    Jerry is allowed inside
+    Kevin is allowed inside
+    Griffin is allowed inside
+    Bryce is not allowed inside
+    
+    TO TRY: have 1,2,3 before each sentence (counter)
+    """
+    
+greeting = "hello my name is Evan"
+
+for char in greeting:
+    if char == "n":
+        continue # will skip the letter n
+    print(char)
+    
+print("Loop is terminated by this point")
+
