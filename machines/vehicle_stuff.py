15b9395451e5151d4e79259613c21edd88473830
Evan Pavone <evanrpavone@gmail.com>
2021-06-28 12:03:41 -0400

    Inheritance and Polymorphism

--- machines/vehicle_stuff.py
+++ machines/vehicle_stuff.py
@@ -1,16 +1,27 @@
-class Car:
+class Vehicle:
 
 	# the color should not be changed outside of the class specification.
 	# this is a class attribute
-	color = 'red'
 	vehicle_counter = 0
 	
 	def __init__(self, body_type, make): # <- !!important!!
 		# specify the attributes of the vehicle here
 		self.vehicle_body = body_type # must use self to be able to use it on objects like car1 and car2
 		self.vehicle_make = make
-		Car.vehicle_counter += 1
+		Vehicle.vehicle_counter += 1
 
 	def get_vehicle_count(self):
-		return Car.vehicle_counter
+		return Vehicle.vehicle_counter
 
+	def drive(self):
+		print("vehicle driving...")
+
+class Truck(Vehicle): # put Vehicle in () to inherit the Vehicle Class
+    
+    def drive(self):
+        print("Truck driving...")
+
+class Motorcycle(Vehicle): # put Vehicle in () to inherit the Vehicle Class
+    
+    def drive(self):
+        print("Motorcycling driving very fast...")
--- vehicle_application.py
+++ vehicle_application.py
@@ -1,4 +1,17 @@
-from machines.vehicle_stuff import Car
+from machines.vehicle_stuff import Vehicle, Truck, Motorcycle
 
-car1 = Car("Sedan", "Subaru")
-print(car1.vehicle_make)
+truck1 = Truck("Big Rig", "Mercedes")
+car1 = Vehicle("SUV", "Subaru")
+moto1 = Motorcycle("Sport", "Yamaha")
+
+print(truck1.get_vehicle_count())
+
+truck1.drive()
+
+for v in [truck1, car1, moto1]:
+    v.drive() # polymorphism
+    
+def perform_tasks(vehicle_object):
+    vehicle_object.drive()
+    
+perform_tasks(car1)
