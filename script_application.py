91002689181b180e9c4944a5bd2d144362b83ef6
Evan Pavone <evanrpavone@gmail.com>
2021-07-06 14:17:28 -0400

    python script files

---
+++ script_application.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("Hello there")
+"""
+Save the file anywhere
+do ls in terminal to list the contents of the directory
+for me its in documents/coding/python/learningpython/script_application.py
+in the terminal do python3 script_application.py
+This will return Hello there
+instead of having python3 in the terminal you actually specify within the file that you'd like to use the Python interpreter to run the code.
+to do this you need to add it at the top of your file... #! /usr/bin/env python3
+you need permissions to run this file  in the terminal-> chmod 755 script_application.py
+in the terminal: ./script_application.py
+"""
