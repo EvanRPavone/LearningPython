bd4dc98665a0898cc5edbb4aeeeea2a3289dcc90
Evan Pavone <evanrpavone@gmail.com>
2021-07-07 09:50:28 -0400

    Modules and using code from other files

---
+++ main_module.py
@@ -0,0 +1,12 @@
+from another_module import greet_user
+# another file is considered as a module
+greet_user()
+"""
+In the command line:
+cd to where your file is
+python3 main_module.py
+
+VOCAB:
+Module - when you have a file that contains code, you should generally refer to it as a module
+Package - just a folder with python files in it
+"""
