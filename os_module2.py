c48b16152589d3a0165d3daa64ac08a1df459cb2
Evan Pavone <evanrpavone@gmail.com>
2021-07-14 10:21:00 -0400

    os module continued

--- os_module2.py
+++ os_module2.py
@@ -2,10 +2,11 @@ import os
 
 for dirpath, dirnames, filenames in os.walk("/Users/evanpavone/Documents/Coding/Python/LearningPython/myfolder"):
     #unpacking
-    print(dirpath)
-    print(dirnames)
-    print(filenames)
-    print(" ------------ ")
+    # print(dirpath)
+    # print(dirnames)
+    # print(filenames)
+    # print(" ------------ ")
+    pass
     
 """
 /Users/evanpavone/Documents/Coding/Python/LearningPython/myfolder - dirpath
@@ -21,3 +22,21 @@ for dirpath, dirnames, filenames in os.walk("/Users/evanpavone/Documents/Coding/
 ['peacock.jpeg'] - filenames
  ------------ 
 """
+
+# os.environ.get("HOME") + "/" + "myfile.txt" /Users/evanpavone/myfile.txt
+
+print(os.path.join(os.environ.get("HOME"), "myfile.txt")) # /Users/evanpavone/myfile.txt
+
+print(os.path.basename("/bin/tools/myfile.txt")) # myfile.txt - used to get basename... thats the file at the directory location given
+
+print(os.path.dirname("/bin/tools/myfile.txt")) # /bin/tools - get the directory name only, not the file
+
+print(os.path.split("/bin/tools/myfile.txt")) # ('/bin/tools', 'myfile.txt') - will give directory name and basename in a tuple 
+
+print(os.path.exists("/bin/tools/myfile.txt")) # False - used to check if the path exists on the computer
+
+print(os.path.isfile("/bin/tools/myfile.txt")) # used to check if file exists in the specified path
+
+print(os.path.isdir("/bin/tools/myfile.txt")) # check if directory exists in the specified path
+
+print(os.path.splitext("/bin/tools/myfile.txt")) # ('/bin/tools/myfile', '.txt') - get file with path and file extension in a tuple
